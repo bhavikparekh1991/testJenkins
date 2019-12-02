c0f191d2e862ee0f2dd65a891559e7a291ba0280
Bhavik.p <Bhavik.p@ISCHLAPHP17131.IDC.Innovasolutions.int>
2019-12-02 19:35:04 +0530

    test

--- testJenkins/src/test/java/testJenkins/testJenkins/AppTest.java
+++ testJenkins/src/test/java/testJenkins/testJenkins/AppTest.java
@@ -15,19 +15,19 @@ public class AppTest
 	@org.testng.annotations.Test
 	public void Jtest() throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "D:\\User profile backup\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "D:\\User profile backup\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		 
 		// Initialize browser
-		WebDriver driver=new ChromeDriver();
+		//WebDriver driver=new ChromeDriver();
 		 
 		// Open facebook
-		driver.get("http://www.facebook.com");
+		//driver.get("http://www.facebook.com");
 		 
 		// Maximize browser
 		 System.out.println("Hello World");
-		driver.manage().window().maximize();
-		Thread.sleep(2000);
-		driver.quit();
+		//driver.manage().window().maximize();
+		//Thread.sleep(2000);
+		//driver.quit();
 		 
 	}
 	 
