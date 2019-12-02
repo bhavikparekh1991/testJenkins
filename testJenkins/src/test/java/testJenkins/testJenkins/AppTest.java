91447f4d380dc4ed177f4fcb9681cf92d6fca5de
Bhavik.p <Bhavik.p@ISCHLAPHP17131.IDC.Innovasolutions.int>
2019-12-02 19:04:34 +0530

    Test

---
+++ testJenkins/src/test/java/testJenkins/testJenkins/AppTest.java
@@ -0,0 +1,34 @@
+package testJenkins.testJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@org.testng.annotations.Test
+	public void Jtest() throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\User profile backup\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		 
+		// Initialize browser
+		WebDriver driver=new ChromeDriver();
+		 
+		// Open facebook
+		driver.get("http://www.facebook.com");
+		 
+		// Maximize browser
+		 
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.quit();
+		 
+	}
+	 
+}
